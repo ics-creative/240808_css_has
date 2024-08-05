66163b6f9b6bfc4839be429f3d1b60e17c178790
ics-nishihara <nishihara@ics-web.jp>
2024-08-05 16:08:54 +0900

    デモ作成

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import "dotenv/config";
+import { globSync } from "glob";
+import { relative, extname } from "path";
+import { fileURLToPath } from "node:url";
+
+const basePath = process.env.BASE_PATH || "/";
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+    minify: false,
+    rollupOptions: {
+      input: {
+        ...Object.fromEntries(
+          globSync("src/**/*.html").map((file) => [
+            relative("src", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+        ),
+      },
+      output: {
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name == "style.scss") {
+            return "assets/style.scss";
+          }
+          return "assets/[name][extname]";
+        },
+      },
+    },
+    target: "esnext",
+    assetsInlineLimit: 0,
+  },
+  base: basePath,
+  server: {
+    port: 3456,
+  },
+});
